d8adeae463b8b6533f62577dd4a41e912d25fab2
nitinsayshe <nitinsayshe@gmail.com>
2024-02-20 23:41:05 +0530

    first push

---
+++ src/handlers/users/userLogin.js
@@ -0,0 +1,22 @@
+const { connectDatabase } = require("db-service/config/db");
+const { User } = require("db-service/models/user");
+const { generateOTP } = require("db-service/utils/helper");
+
+module.exports = exports.handler = async (event) => {
+
+  try {
+    await connectDatabase();
+    const body = JSON.parse(event.body);
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify("userObj"),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+}
